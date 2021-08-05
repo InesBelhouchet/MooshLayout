c79255117dbfbd18b10d12a6dfdc1234d40b290f
Ines Belhouchet <inesbelhouchet@Iness-MacBook-Pro.local>
2021-08-05 22:25:34 +0200

    initial commit

---
+++ app/src/main/java/com/uiapp/mooshlayout/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.uiapp.mooshlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.uiapp.mooshlayout.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
